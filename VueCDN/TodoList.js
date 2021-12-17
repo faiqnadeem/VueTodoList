9feade529957cefadc4125e59327a3efe8aa2ae5
chfaiqnadeem <chfaiqnadeem@gmail.com>
2021-12-17 13:10:42 +0500

    Added watcher for notification and update counter

--- VueCDN/TodoList.js
+++ VueCDN/TodoList.js
@@ -1,9 +1,11 @@
 new Vue({
     el  : '#vue-app',
     data: {
-        todoItem: {title:'', date:''},
-        formShow: false,
-        todoList: [
+        todoItem    : {title:'', date:''},
+        formShow    : false,
+        notification: '',
+        counter     : 0,
+        todoList    : [
             {title:'Make a table', date:'2021-11-18T11:40', status: false},
             {title:'make a list', date:'2021-10-18T1:20', status: true},
             {title:'make buttons', date:'2021-9-18T2:00', status: false},
@@ -29,5 +31,13 @@ new Vue({
         todoListReversed() {
             return this.todoList.slice(0).reverse()
         }
+    },
+    watch: {
+        todoList: {
+            handler: function(){
+                this.notification = 'TodoList Updated / ' + this.counter++
+            },
+            deep: true
+        },
     }
 });
